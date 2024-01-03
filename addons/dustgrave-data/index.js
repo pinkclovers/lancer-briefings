ce0299a18a0f454f1ac37c4bea373d101ff9ad97
Pink <thepixelatedpotato32@gmail.com>
2024-01-03 10:55:49 -0500

    Add files via upload

---
+++ addons/dustgrave-data/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const data = {
+  lcp_manifest: require('./lib/lcp_manifest.json'),
+  core_bonuses: require('./lib/core_bonuses.json'),
+  frames: require('./lib/frames.json'),
+  pilot_gear: require('./lib/pilot_gear.json'),
+  reserves: require('./lib/reserves.json'),
+  systems: require('./lib/systems.json'),
+  talents: require('./lib/talents.json'),
+  weapons: require('./lib/weapons.json'),
+  npc_features: require('./lib/npc_features.json'),
+  npc_templates: require('./lib/npc_templates.json'),
+};
+
+module.exports = data;
