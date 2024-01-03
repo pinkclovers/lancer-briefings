4e8ef6f08287676ceb77062f169b482f5f06453c
Pink <thepixelatedpotato32@gmail.com>
2024-01-02 23:15:58 -0500

    Add files via upload

---
+++ node_modules/lancer-ktb-data/index.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const data = {
+  bonds: require('./lib/bonds.json'),
+  frames: require('./lib/frames.json'),
+  lcp_manifest: require('./lib/lcp_manifest.json'),
+  statuses: require('./lib/statuses.json'),
+  systems: require('./lib/systems.json'),
+  tags: require('./lib/tags.json'),
+  talents: require('./lib/talents.json'),
+  weapons: require('./lib/weapons.json'),
+}
+
+module.exports = data;
---
+++ node_modules/lancer-nrfaw-data/index.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const data = {
+  frames: require('./lib/frames.json'),
+  lcp_manifest: require('./lib/lcp_manifest.json'),
+  pilot_gear: require('./lib/pilot_gear.json'),
+  systems: require('./lib/systems.json'),
+  talents: require('./lib/talents.json'),
+  weapons: require('./lib/weapons.json'),
+}
+
+module.exports = data;
