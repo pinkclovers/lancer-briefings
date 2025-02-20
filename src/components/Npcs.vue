000bb571af6e0faf7e5b27fc39ddb2e281edc93e
Pink <thepixelatedpotato32@gmail.com>
2025-02-20 17:07:20 -0500

    Update Npcs.vue

--- src/components/Npcs.vue
+++ src/components/Npcs.vue
@@ -12,7 +12,8 @@
 						<div class="name">
           					<!--<h1>Name or Legal Alias</h1>-->
          					<h2>{{ npc.name }}</h2>
-						<h1>{{ npc.pronoun }}</h1> \n
+						<h1>{{ npc.pronoun }}</h1>
+						<br />
         					</div>
 					</div>
 					<h1>{{ npc.title }}</h1> 
