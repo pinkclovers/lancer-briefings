be245fcd3836da2a4071b2be2e210f811ad7af9f
Pink <thepixelatedpotato32@gmail.com>
2025-02-20 16:41:57 -0500

    Update NpcsFull.vue

--- src/components/NpcsFull.vue
+++ src/components/NpcsFull.vue
@@ -9,12 +9,13 @@
       				<div class="gear-row">
 						<div class="name">
           					<!--<h1>Name or Legal Alias</h1>-->
-         					<h2>{{ npc.title }}</h2>
+         					<h2>{{ npc.name }}</h2>
+						<h1>{{ npc.pronoun }}</h1>
         				</div>
 					</div>
 					<div class="npc">
-						<h1>{{ npc.location }}</h1> 
-							<h1>// {{ npc.time }}</h1>
+						<h1>{{ npc.title }}</h1> 
+						<h1>// {{ npc.location }}</h1>
 						<div class="preview">
 							{{ getPreview }}
 						</div>
